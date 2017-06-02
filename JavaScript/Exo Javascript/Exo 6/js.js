6fef73f7425f7c55becebe07e7fd13eabc253975
Mesmaeker <mike-mesmaeker@hotmail.com>
2017-06-02 15:54:21 +0200

    Delete js.js

--- JavaScript/Exo Javascript/Exo 6/js.js
+++
@@ -1,5 +0,0 @@
-function myFunction() {
-  var chaton = document.getElementById("premiernombre").value;
-  var chatone = document.getElementById("deuxiemenombre").value;
-  alert(parseInt(chaton)%parseFloat(chatone));
-}
